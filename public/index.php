148ca13fdfb2e75b502b555cffb8124c282cd1ee
avinmaster <oybek.odilov.dev@gmail.com>
2022-05-20 00:33:34 +0500

    Fixed CORS

--- public/index.php
+++ public/index.php
@@ -10,7 +10,8 @@ require '../src/controllers/KeysController.php';
 $keys_controller = new KeysController();
 
 router('GET', '^/$', [$keys_controller, 'index']);
-router('GET', '^/(?<key>\d+)$', [$keys_controller, 'delete']);
+// Сделал GET потому что с другими методами выдаются CORS ошибки
+router('GET', '^/(?<key>(?s).*+)$', [$keys_controller, 'delete']);
 
 header("HTTP/1.0 404 Not Found");
 echo '404 Not Found';
